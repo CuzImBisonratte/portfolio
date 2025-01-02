91328cd0fe500495298d9a20c45da0297b40d413
Konstantin Protzen <code@kondev.de>
2025-01-02 21:29:37 +0100

    🐛 Fix font loading path

--- admin/editor.php
+++ admin/editor.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="/admin/css/admin.css">
-    <link rel="stylesheet" href="/admin/css/fonts.css">
     <link rel="stylesheet" href="/admin/css/editor.css">
     <link rel="stylesheet" href="/css/portfolio.css">
+    <link rel="stylesheet" href="/css/fonts.css">
     <link rel="stylesheet" href="/css/base.css">
 </head>
 
