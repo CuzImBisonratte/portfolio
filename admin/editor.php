8cf631520b41ad07c88b711afa1b78f06069e6be
Konstantin Protzen <code@kondev.de>
2025-01-02 14:16:44 +0100

    🚧 Add WIP navbar

---
+++ admin/editor.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/admin/css/admin.css">
+    <link rel="stylesheet" href="/css/base.css">
+</head>
+
+<body>
+    <nav>
+        <div class="nav-container">
+            <a class="nav-item" href="/admin/index.php">Home</a>
+            <a class="nav-item" href="/admin/editor.php">Editor</a>
+            <a class="nav-item" href="/admin/logout.php">Logout</a>
+        </div>
+    </nav>
+    <main>
+        <div class="image-selector"></div>
+        <div class="wysiwyg-editor"></div>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
