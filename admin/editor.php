786b6fa9e916a7fb6e2405d5f4f750553e90407f
Konstantin Protzen <code@kondev.de>
2025-02-11 11:13:13 +0100

    ✨ Use DB for image manager

--- admin/editor.php
+++ admin/editor.php
@@ -38,10 +38,10 @@ while ($stmt->fetch()) {
     $clusters[$cluster['id']] = $cluster;
 }
 $stmt->close();
-$stmt = $con->prepare("SELECT id, alt, camera, artist, time FROM image WHERE page_id = ?");
+$stmt = $con->prepare("SELECT id, alt, camera, artist, time, fileformat FROM image WHERE page_id = ?");
 $stmt->bind_param('s', $page['id']);
 $stmt->execute();
-$stmt->bind_result($image['id'], $image['alt'], $image['camera'], $image['artist'], $image['time']);
+$stmt->bind_result($image['id'], $image['alt'], $image['camera'], $image['artist'], $image['time'], $image['fileformat']);
 while ($stmt->fetch()) {
     $images[$image['id']] = $image;
 }
@@ -57,8 +57,6 @@ foreach ($clusters as $cluster) {
     }
 }
 $stmt->close();
-
-echo json_encode($clusters);
 
 ?>
 
@@ -311,15 +309,15 @@ echo json_encode($clusters);
                 <?php
 
                 // Loop through all images in pageconfig
-                foreach ($pageConfig['images'] as $key => $image) {
-                    echo '<div class="mediaManagerImageContainer" id="' . $key . '">';
-                    echo '<img src="/admin/pages/' . $_GET['page'] . '/images/' . str_replace(".JPG", ".webp", $image['src']) . '" alt="" onclick="mediaManager.click(\'' . $key . '\')">';
-                    echo '<a class="image-delete" href="javascript:mediaManager.deleteImage(\'' . $key . '\')">';
+                foreach ($images as $index => $image) {
+                    echo '<div class="mediaManagerImageContainer" id="' . $index . '">';
+                    echo '<img src="/admin/images/' . $_GET['page'] . '/' . $index . ".webp" . '" alt="" onclick="mediaManager.click(\'' . $index . '\')">';
+                    echo '<a class="image-delete" href="javascript:mediaManager.deleteImage(\'' . $index . '\')">';
                     echo '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">';
                     echo '<path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />';
                     echo '</svg>';
                     echo '</a>';
-                    echo '<a class="image-info" href="javascript:mediaManager.editMetadata(\'' . $key . '\')">';
+                    echo '<a class="image-info" href="javascript:mediaManager.editMetadata(\'' . $index . '\')">';
                     echo '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">';
                     echo '<path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />';
                     echo '</svg>';
